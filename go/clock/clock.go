6b958b02b41f8e256fef885a386361d3a68b0ec0
Richard Hao <richard@0xdev.dev>
2023-06-22 09:11:15 +0800

    chore(go): use Euclid Reminder to improve the Clock example

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,53 +2,37 @@ package clock
 
 import (
 	"fmt"
-	"strconv"
-	"time"
+	"math"
 )
 
 // Define the Clock type here.
 type Clock struct {
-	h int
 	m int
 }
 
+const MINUTE_PER_HOUR = 60
+const MINUTE_PER_DAY = MINUTE_PER_HOUR * 24
+
 func New(h, m int) Clock {
-	t := newTime(h, m)
-	return Clock{h: t.Hour(), m: t.Minute()}
+	minutes := remEuclid(h*MINUTE_PER_HOUR+m, MINUTE_PER_DAY)
+
+	return Clock{m: minutes}
 }
 
 func (c Clock) Add(m int) Clock {
-	duration := time.Duration(m) * time.Minute
-	t := c.time().Add(duration)
-	return New(t.Hour(), t.Minute())
+	return New(0, c.m+m)
 }
 
 func (c Clock) Subtract(m int) Clock {
-	duration := time.Duration(m) * time.Minute
-	t := c.time().Add(-duration)
-	return New(t.Hour(), t.Minute())
+	return New(0, c.m-m)
 }
 
 func (c Clock) String() string {
-	h, m, _ := c.time().Clock()
-	return fmt.Sprintf("%02d:%02d", h, m)
+	hours := c.m / MINUTE_PER_HOUR
+	minutes := c.m % MINUTE_PER_HOUR
+	return fmt.Sprintf("%02d:%02d", hours, minutes)
 }
 
-func (c Clock) time() time.Time {
-	return newTime(c.h, c.m)
-}
-
-func newTime(h, m int) time.Time {
-	t := time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC)
-	durationFromHour := time.Duration(h) * time.Hour
-	durationFromMinute := time.Duration(m) * time.Minute
-	t = t.Add(durationFromHour).Add(durationFromMinute)
-	return t
-}
-
-func convertDurationToString(d int) string {
-	if d < 10 {
-		return fmt.Sprintf("0%d", d)
-	}
-	return strconv.Itoa(d)
+func remEuclid(a, b int) int {
+	return a - b*int(math.Floor(float64(a)/float64(b)))
 }
