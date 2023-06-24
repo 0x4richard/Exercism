55e45e407483a3795aec4652f61dd055777a0cf6
Richard Hao <richard@0xdev.dev>
2023-06-24 17:26:32 +0800

    refactor: reduce to_lowercase calls because case changes are expensive in terms of time

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -7,7 +7,7 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet
 
     for w in possible_anagrams {
         let sw = sort_word(w);
-        if sorted_workd == sw && word.to_string().to_lowercase() != w.to_string().to_lowercase() {
+        if sorted_workd == sw && to_lowercase(word) != to_lowercase(w) {
             set.insert(w.as_ref());
         }
     }
@@ -15,8 +15,20 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet
     set
 }
 
+fn to_lowercase_fn(c: char) -> String {
+    if c.is_lowercase() {
+        c.to_string()
+    } else {
+        c.to_lowercase().to_string()
+    }
+}
+
+fn to_lowercase(word: &str) -> String {
+    word.chars().map(to_lowercase_fn).collect::<String>()
+}
+
 fn sort_word(word: &str) -> String {
-    let mut sorted_word = word.to_lowercase().chars().collect::<Vec<char>>();
+    let mut sorted_word = to_lowercase(word).chars().collect::<Vec<char>>();
     sorted_word.sort_unstable();
     sorted_word.into_iter().collect::<String>()
 }
