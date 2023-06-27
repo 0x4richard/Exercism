63f9e033e252a28e211d1c516f9161519e80f207
Richard Hao <richard@0xdev.dev>
2023-06-27 20:02:55 +0800

    refactor(rust): refactor the implementation of Speac age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -16,8 +16,10 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
+    const PERIOD: f64 = 1.0;
+
     fn years_during(d: &Duration) -> f64 {
-        d.earth_year
+        d.earth_year / Self::PERIOD
     }
 }
 
@@ -31,38 +33,24 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 0.2408467
-    }
+    const PERIOD: f64 = 0.2408467;
 }
 impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 0.61519726
-    }
+    const PERIOD: f64 = 0.61519726;
 }
 impl Planet for Earth {}
 impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 1.8808158
-    }
+    const PERIOD: f64 = 1.8808158;
 }
 impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 11.862615
-    }
+    const PERIOD: f64 = 11.862615;
 }
 impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 29.447498
-    }
+    const PERIOD: f64 = 29.447498;
 }
 impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 84.016846
-    }
+    const PERIOD: f64 = 84.016846;
 }
 impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.earth_year / 164.79132
-    }
+    const PERIOD: f64 = 164.79132;
 }
